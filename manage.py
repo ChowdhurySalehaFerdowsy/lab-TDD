b4fc61be8305b75730a2c183962cdabf29ba4870
ChowdhurySalehaFerdowsy <shipu9300@gmail.com>
2023-12-04 16:06:00 -0500

    part 3

--- manage.py
+++ manage.py
@@ -1,9 +1,16 @@
 # manage.py
 
 from flask.cli import FlaskGroup
-from src import app
+from src import app, db #new-part 3
 
 cli = FlaskGroup (app)
+
+# new-part -3
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__=='__main__':
     cli()
--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,10 @@
 # src/__init__.py
-import os  # new
+import os  # new-part3
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new-part3
+
+
 
 # instantiate the app
 app = Flask (__name__)
@@ -13,6 +16,22 @@ app.config.from_object('src.config.DevelopmentConfig')  # new
 app_settings = os.getenv('APP_SETTINGS')  # new
 app.config.from_object(app_settings)      # new
 
+# instantiate the db
+db = SQLAlchemy(app)  # new-part3
+
+# model-new class for part -3
+class User(db.Model):  # new-part3
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
 class Ping(Resource):
     def get(self):
         return {
@@ -22,5 +41,5 @@ class Ping(Resource):
 api.add_resource(Ping,'/ping')
 
 
-import sys
+
 
